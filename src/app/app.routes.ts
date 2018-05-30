4581f97dcf29e165da2ced4a1e2aa9df33b7bece
Alexey <asdalexey@yandex.ru>
2018-05-30 23:40:48 +0300

    init commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { RouterModule } from '@angular/router';
+
+export const routes = [
+  // { path: '', loadChildren: './home/home.module#HomeModule' },
+];
+
+export const appRoutes = RouterModule.forRoot(routes);
