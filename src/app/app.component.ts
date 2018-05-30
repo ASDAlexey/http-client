e0f929793320dc7ca65aad5c2a1a829f4975d80a
Alexey <asdalexey@yandex.ru>
2018-05-30 23:54:57 +0300

    #1 add first http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PeopleService } from './people.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  people$;
+
+  constructor(private peopleService: PeopleService) {
+
+  }
+
+  fetchPeople() {
+    this.people$ = this.peopleService.fetchPeople();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { appRoutes } from './app.routes';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     appRoutes,
   ],
   declarations: [
